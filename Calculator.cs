c537230df5478bc8ac1febf0320fefe92f6bf98d
Crisasster <122651949+Crisasster@users.noreply.github.com>
2023-02-18 23:51:57 -0500

    Add files via upload

---
+++ Calculator.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KnowledgeCheck
+{
+    public class Calculator
+    {
+        public int Add(int first, int second)
+        {
+            return first + second;
+        }
+
+        public int Subtract(int first, int second)
+        {
+            return first - second;
+        }
+
+        public static int Multiply(int first, int second)
+        {
+            return first * second;
+        }
+
+        public double Divide(double first, double second)
+        {
+            return first / second;
+        }
+
+        internal int Multiply(double mulNumOne, double mulNumTwo)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
